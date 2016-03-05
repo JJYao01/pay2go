52a628a11d53d91c5f6bd8e676d68f949c56bc8e
JJYao01 <wind791028@gmail.com>
2016-03-05 14:31:08 +0800

    pay2go period send success

--- pay2go/period/urls.py
+++ pay2go/period/urls.py
@@ -4,6 +4,7 @@ from period import views
 
 urlpatterns = [
     url(r'^checkValue/$', views.checkValue, name='checkValue'),
+    url(r'^receive/$', views.receive, name='receive'),
     url(r'^period/$', views.main, name='main'),
     url(r'^$', views.main, name='main'),
     
--- pay2go/period/views.py
+++ pay2go/period/views.py
@@ -8,20 +8,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    print('main')
     return render(request, 'period/main.html', {})
 
 def checkValue(request):
 #m43Yy51kOifGzcYGTIvA6DCpiy3iE6K0
 #HashIV=aNZJT03HjjnSvmzp
+#+'&TimeStamp='+ timeStamp
     hashKey = 'HashKey=m43Yy51kOifGzcYGTIvA6DCpiy3iE6K0'
     hashIV = 'HashIV=aNZJT03HjjnSvmzp'
     if request.is_ajax():
-        request_data = request.POST    
-        datetimeNow=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')        
-        TimeStamp=str(int(time.mktime(time.strptime(datetimeNow, '%Y-%m-%d %H:%M:%S'))))        
-        checkValue = hashKey +'&'+ request_data['checkValue']+'&TimeStamp='+ TimeStamp +'&'+ hashIV        
-        print(checkValue)
+        request_data = request.POST        
+        #datetimeNow=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')        
+        #timeStamp=str(int(time.mktime(time.strptime(datetimeNow, '%Y-%m-%d %H:%M:%S'))))      
+        checkValue = hashKey +'&'+ request_data['checkValue'] +'&'+ hashIV        
         encrypted = hashlib.sha256(bytes(checkValue, encoding="utf-8")).hexdigest()       
         return HttpResponse(encrypted.upper())
-    #
+    
+def receive(request):
+    if request['Status'] == 'SUCCESS':
+        print(request)
+    return render(request, 'period/main.html', {})
